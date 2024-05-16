4c9e64e4ececdb9d2cf3b745ecb9e75c19b6d6d9
syedalirazazaidi <aaliraza007@hotmail.com>
2024-05-16 10:40:16 +0500

    add selected button

--- list/src/components/transfirlist.tsx
+++ list/src/components/transfirlist.tsx
@@ -16,26 +16,32 @@ export default function Transfirlist() {
   const [listmy, setStateList] = useState<ITemType[]>([]);
   const [bgColor, setbgColor] = useState<string>("");
   const [bgId, setbgID] = useState<number>();
+  const [moveLeft, setmoveLeft] = useState(0);
+  const [moveRight, setmoveRight] = useState(0);
 
   const listIDClick = (newlist: any, id: number) => {
     const itemToUpdate = items.find((item) => item.id === id);
-  
+
     if (itemToUpdate) {
       setbgID(itemToUpdate.id);
       setbgColor("red");
     }
-  
+
     setStateList((prevListMy) => {
       const idExists = prevListMy.some((item) => item.id === id);
-     
       if (!idExists) {
         return [...prevListMy, newlist];
       }
-  
-      return prevListMy; 
+      return prevListMy;
     });
   };
-  
+  const movetoLeft = () => {
+    setmoveRight(0);
+  };
+  const movetoRight = () => {
+    setmoveRight(1);
+  };
+
   return (
     <div className="flex justify-center gap-5 items-center">
       <div className="bg-gray-200/50 rounded-xl w-[148px] h-60">
@@ -59,10 +65,39 @@ export default function Transfirlist() {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center gap-2 ">
-        <button className="bg-gray-200 rounded-full w-7 pb-1">&laquo; </button>
-        <button className="bg-gray-200 rounded-full w-7 pb-1">&raquo; </button>
+        <button
+          className="bg-gray-200 rounded-full w-7 pb-1"
+          onClick={movetoRight}
+        >
+          &laquo;{" "}
+        </button>
+        <button
+          className="bg-gray-200 rounded-full w-7 pb-1"
+          onClick={movetoLeft}
+        >
+          &raquo;{" "}
+        </button>
       </div>
-      <div className="bg-gray-200/50 rounded-xl w-[148px] h-60"></div>
+      <div className="bg-gray-200/50 rounded-xl w-[148px] h-60">
+        <div className="flex flex-col m-4 gap-4 ">
+          {moveRight === 1 && (
+            <div>
+              {listmy.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <button
+                      className={` bg-gray-200 w-[90px] py-1  text-[12px] font-semibold text-white m-2 rounded-lg`}
+                      onClick={() => listIDClick(item, item.id)}
+                    >
+                      {item.list}
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
